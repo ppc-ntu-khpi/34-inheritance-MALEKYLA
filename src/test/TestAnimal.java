e7037eace5b37896c2b9a8f36bfee653061b5486
MALEKYLA <58940885+MALEKYLA@users.noreply.github.com>
2021-02-09 13:07:48 +0200

    Update TestAnimal.java

--- src/test/TestAnimal.java
+++ src/test/TestAnimal.java
@@ -1,15 +1,31 @@
 package test;
 
-import domain.Cat;
+import domain.Eagle;
 
+ /**
+ * The class for test class Eagle
+ */ 
 public class TestAnimal {
 
-    public static void main(String[] args) {
-        Cat cat = new Cat("Thomas");
-        System.out.println(cat);
-        cat.hunt();
-        cat.eat();
-        cat.speak();
-        cat.play();
+
+/** 
+ *
+ * Method for test Eagle
+ *
+ * @param args  the args
+ */
+    public static void main(String[] args) { 
+
+        
+        Eagle eagle = new Eagle("Socil Sapsan");
+        
+        System.out.println(eagle);
+        
+        eagle.eat();
+        eagle.fly();
+        eagle.hunt();
+        eagle.sleep();
+        eagle.speak();
     }
 }
+

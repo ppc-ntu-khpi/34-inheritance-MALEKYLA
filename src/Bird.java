4fed9210d42855ccc89944366e949031b02466a6
MALEKYLA <58940885+MALEKYLA@users.noreply.github.com>
2021-02-09 13:05:35 +0200

    Add files via upload

---
+++ src/Bird.java
@@ -0,0 +1,30 @@
+package domain;
+
+
+ /**
+ * The class Bird extends Animal
+ */ 
+public class Bird extends Animal {
+
+
+/** 
+ *
+ * Method for fly
+ *
+ */
+    public void fly() { 
+
+        System.out.println("Bird fly...");
+    }
+
+
+/** 
+ *
+ * Method for hunt
+ *
+ */
+    public void hunt() { 
+
+        System.out.println("Bird nuhting...");
+    }
+}
